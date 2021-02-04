7c537818a860bd82a8cf1e39e2257790273fbaea
José Arturo Bustamante Lazcano <arturobl00@msn.com>
2021-02-04 09:06:12 -0600

    Clase1

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Primer día con PHP</title>
+</head>
+<body>
+
+    <!-- Comentarios de la clase de hoy
+    Hoy conocimos como usar wampserver, la carpeta de proyectos, como las nombraremos, como vemos el localhost,
+    una introduccion a php y html. Las primeras etiquetas de html que fueron <head> <body> <title> <h1 al h6>, <p>, <br> y <hr> -->
+
+    <h1>Hola a todos soy un Encabezado h1</h1>
+    <h1>Mostrando un ejemplo de tipos de encabezado</h1>
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+
+    <h1>Etiqueta parrafo p</h1>
+    <p>Además, no sé si es necesario decir que la idea de que una literatura
+     debe definirse por los rasgos diferenciales del país que la produce es
+    una idea relativamente nueva; también es nueva y arbitraria la idea de que
+    los escritores deben buscar temas de sus países. Sin ir más lejos, creo que
+    Racine ni siquiera hubiera entendido a una persona que le hubiera negado su
+    derecho al título de poeta francés por haber buscado temas griegos y latinos.
+    Creo que Shakespeare se habría asombrado si hubieran pretendido limitarlo a
+    temas ingleses, y si le hubiesen dicho que, como inglés, no tenía derecho a
+    escribir Hamlet, de tema escandinavo, o Macbeth, de tema escocés. El culto
+    argentino del color local es un reciente culto europeo que los nacionalistas
+    deberían rechazar por foráneo.</p>
+
+    <p style="color:blue;">Hola este es el ejemplo que propongo sobre el uso de los parrafos.
+    Esta estiqueta se usa mucho en el diseño Front End.</p>
+
+    <p style="color:red;">Hola este es el ejemplo que propongo sobre el uso de los parrafos.</p>
+    <p style="color:red;">Esta estiqueta se usa mucho en el diseño Front End.</p>
+
+    <p style="color:green;">Hola este es el ejemplo que propongo sobre el uso de los parrafos.<br>
+    Esta estiqueta se usa mucho en el diseño Front End.</p>
+
+    <h1>Etiqueta hr pone una linea ejemplo tipo Wikipedia</h1>
+    <h3>Primeras especificaciones de HTML</h3>
+    <hr>
+    <p>Tim Berners-Lee (TBL) en 19912​3​ describe 18 elementos que incluyen el diseño inicial y relativamente simple de HTML.<br>
+    Trece de estos elementos todavía existen en HTML 4.4​ Berners-Lee consideraba el HTML una ampliación de SGML, pero no fue
+    formalmente reconocida como tal hasta la publicación a mediados de 1993, por la IETF (en español: Grupo de Trabajo de Ingeniería
+    de Internet), de una primera proposición para una especificación del HTML: el borrador del Hypertext Markup Language de Berners-Lee
+    y Dan Connolly, que incluía una Definición de Tipo de Documento SGML para definir la gramática.5​ El borrador expiró a los seis meses,
+    pero fue notable por su reconocimiento de la etiqueta propia del navegador Mosaic usada para insertar imágenes sin cambio de línea,
+    que reflejaba la filosofía del IETF de basar estándares en prototipos con éxito.6​De la misma manera, el borrador competidor de Dave
+    Raggett HTML+ (Hypertext Markup Format) (Formato de Marcaje de Hipertexto), de finales de 1993, sugería estandarizar características
+    ya implementadas, como las tablas.​</p>
+</body>
+</html>
